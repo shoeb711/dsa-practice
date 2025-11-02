e19b42568e9b51a6591f90f76743064904836143
Shoeb Shaikh <shaikhshoeb711@gmail.com>
2025-11-02 18:31:21 +0530

    areThereDuplicates using two-pointer

--- areThereDuplicates.js
+++ areThereDuplicates.js
@@ -1,16 +1,39 @@
-function areThereDuplicates(...params) {
-  let obj = {};
+// using frequency counter
 
-  for (let char of params) {
-    if (obj[char]) {
+// function areThereDuplicates(...params) {
+//   let obj = {};
+
+//   for (let char of params) {
+//     if (obj[char]) {
+//       return true;
+//     } else {
+//       obj[char] = 1;
+//     }
+//   }
+
+//   return false;
+// }
+
+// using two pointer
+
+function areThereDuplicates(...params) {
+  const sortedArr = params.sort((a, b) => a - b);
+  console.log("sortedArr", sortedArr);
+  let first = 0;
+  let next = 1;
+  const last = params.length - 1;
+
+  while (first < last) {
+    if (params[first] === params[next]) {
       return true;
     } else {
-      obj[char] = 1;
+      first++;
+      next++;
     }
   }
 
   return false;
 }
 
-const res = areThereDuplicates("a", "b", "c");
+const res = areThereDuplicates(1, 2, 3, 4, 5, 6, 1);
 console.log(res);
